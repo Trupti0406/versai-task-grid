9e1a3c7f2745bb5ee05ee6217e9fde96aa1e7f3f
Trupti <truptivijayyadav27@gmail.com>
2024-11-08 21:17:01 +0530

    Hover adjustments

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -5,9 +5,13 @@ const Card = ({ src, title, description }) => {
   return (
     <div className="card">
       <img src={src} alt={title} className="card-image" />
+      <h3 className="card-title">{title}</h3>
       <div className="card-overlay">
-        <h3 className="card-title">{title}</h3>
-        <p className="card-description">{description}</p>
+        <div className="card-description">
+          <h3>{title}</h3>
+          <br />
+          {description}
+        </div>
       </div>
     </div>
   );
